c720c56677c317ec61bacded4d3147fe6f8a3fec
x-anish-y <anishsasmal5481@gmail.com>
2025-07-15 00:27:35 +0530

    Add files via upload

---
+++ V86/main.js
@@ -0,0 +1,28 @@
+// // const { createServer } = require('node:http');
+// // const fs = require("fs")
+// import http from "http";
+
+// const hostname = '127.0.0.1';
+// const port = 3001;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });   
+
+
+
+// import {a, b, d} from "./mymodule.js"
+// console.log(a, b, d)
+// import obj from "./mymodule.js"
+// console.log(obj)
+import anish from "./mymodule.js"
+console.log(anish)
+
+// const a = require("./mymodule2.js")
+// console.log(a)
