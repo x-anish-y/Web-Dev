bd6a22545e5c3e7fd33c6af4a52a18706fd0eb13
x-anish-y <anishsasmal5481@gmail.com>
2026-02-08 15:18:21 +0530

    Add files via upload

---
+++ V135/my-app/app/about/page.js
@@ -0,0 +1,24 @@
+"use client"
+import React from 'react'
+
+const about = () => {
+    return (
+        <div>
+            <div className='container'>
+                About
+                <style jsx>{
+                // <style jsx global>{
+                    `.container{
+                    background-color: yellow;
+                    color: blue;                
+                    }`
+                }
+                </style>
+            </div>
+            
+            <div className="container">I am good</div>
+        </div>
+    )
+}
+
+export default about
